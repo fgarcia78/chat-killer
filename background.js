bd5f013f1fb651d43ae8914d6600444e8077adef
Fernando Garcia <fgarcia78@gmail.com>
2013-08-26 02:18:49 -0600

    Deleted not used files.

--- background.js
+++
@@ -1,7 +0,0 @@
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log('Killing chat box...');
-  chrome.tabs.executeScript({
-    file: 'kill-chat.js'
-  });
-});
